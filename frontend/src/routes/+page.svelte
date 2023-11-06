f30dd0ee324168d95071ad4b62f3997e3bc28457
th2619 <breeze_th@naver.com>
2023-11-06 14:28:41 +0900

    socket io test

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,105 +1,126 @@
 <script>
-  import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
+    import io from 'socket.io-client';
 
-  let videoElement;
-  let canvasElement;
-  let canvasCtx;
-  let camera;
-  let isCameraConnected = false;
+	let videoElement;
+	let canvasElement;
+	let canvasCtx;
+	let camera;
+	let isCameraConnected = false;
+	let ws;
 
-  onMount(() => {
-      canvasElement = document.createElement('canvas');
-      videoElement.parentNode.insertBefore(canvasElement, videoElement);
-      canvasCtx = canvasElement.getContext('2d');
-      canvasElement.width = videoElement.offsetWidth;
-      canvasElement.height = videoElement.offsetHeight;
+	onMount(() => {
+		canvasElement = document.createElement('canvas');
+		videoElement.parentNode.insertBefore(canvasElement, videoElement);
+		canvasCtx = canvasElement.getContext('2d');
+		canvasElement.width = videoElement.offsetWidth;
+		canvasElement.height = videoElement.offsetHeight;
 
-      const faceMesh = new FaceMesh({
-        locateFile: (file) => {
-          return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
-        }
-      });
+		const faceMesh = new FaceMesh({
+			locateFile: (file) => {
+				return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+			}
+		});
 
-      faceMesh.setOptions({
-        maxNumFaces: 1,
-        refineLandmarks: true,
-        minDetectionConfidence: 0.5,
-        minTrackingConfidence: 0.5
-      });
+		faceMesh.setOptions({
+			maxNumFaces: 1,
+			refineLandmarks: true,
+			minDetectionConfidence: 0.5,
+			minTrackingConfidence: 0.5
+		});
 
-      faceMesh.onResults(onResults);
+		faceMesh.onResults(onResults);
+		camera = new Camera(videoElement, {
+			onFrame: async () => {
+				await faceMesh.send({ image: videoElement });
+			}
+		});
 
-      camera = new Camera(videoElement, {
-        onFrame: async () => {
-          await faceMesh.send({image: videoElement});
-        },
-      });
-  });
+		ws = io('http://localhost:3000');
+        ws.on('connect', function () {
+            console.log('Connected to the WebSocket server!');
+        });
+        ws.on('eventFromServer', function (message) {
+            console.log('Received message from server:', message);
+        });
+        ws.on('disconnect', function () {
+            console.log('Disconnected from the WebSocket server.');
+        });
+	});
 
-  function connectCamera() {
-      camera.start();
-      isCameraConnected = true;
-      videoElement.style.visibility = "hidden";
-  }
+	function connectCamera() {
+		camera.start();
+		isCameraConnected = true;
+		videoElement.style.visibility = 'hidden';
 
-  function disconnectCamera() {
-      if (videoElement.srcObject) {
-          videoElement.srcObject.getTracks().forEach(track => track.stop());
-      }
-      isCameraConnected = false;
-      videoElement.style.visibility = "hidden";
-  }
+		if (!ws || ws.readyState === WebSocket.CLOSED) {
+			ws = new WebSocket('ws://localhost:3000');
+			ws.onopen = function () {
+				console.log('Connected to the WebSocket server!');
+				ws.send(JSON.stringify({ status: 'camera_connected' }));
+			};
+		}
+	}
 
-  function toggleCamera() {
-      if (isCameraConnected) {
-          disconnectCamera();
-      } else {
-          connectCamera();
-      }
-  }
+	function disconnectCamera() {
+		if (videoElement.srcObject) {
+			videoElement.srcObject.getTracks().forEach((track) => track.stop());
+		}
+		isCameraConnected = false;
+		videoElement.style.visibility = 'hidden';
+	}
 
-  function onResults(results) {
-      canvasCtx.save();
-      canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-      canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
-      if (results.multiFaceLandmarks) {
-        for (const landmarks of results.multiFaceLandmarks) {
-          drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION, {color: '#C0C0C070', lineWidth: 1});
-          drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, {color: '#FF3030'});
-          drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, {color: '#FF3030'});
-          drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_IRIS, {color: '#FF3030'});
-          drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYE, {color: '#30FF30'});
-          drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, {color: '#30FF30'});
-          drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_IRIS, {color: '#30FF30'});
-          drawConnectors(canvasCtx, landmarks, FACEMESH_FACE_OVAL, {color: '#E0E0E0'});
-          drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, {color: '#E0E0E0'});
-        }
-      }
-      canvasCtx.restore();
-  }
+	function toggleCamera() {
+		if (isCameraConnected) {
+			disconnectCamera();
+		} else {
+			connectCamera();
+		}
+	}
 
+	function onResults(results) {
+		canvasCtx.save();
+		canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+		canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+		if (results.multiFaceLandmarks) {
+			for (const landmarks of results.multiFaceLandmarks) {
+				drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION, {
+					color: '#C0C0C070',
+					lineWidth: 1
+				});
+				drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, { color: '#FF3030' });
+				drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, { color: '#FF3030' });
+				drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_IRIS, { color: '#FF3030' });
+				drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYE, { color: '#30FF30' });
+				drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, { color: '#30FF30' });
+				drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_IRIS, { color: '#30FF30' });
+				drawConnectors(canvasCtx, landmarks, FACEMESH_FACE_OVAL, { color: '#E0E0E0' });
+				drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, { color: '#E0E0E0' });
+			}
+
+			if (ws && ws.readyState === WebSocket.OPEN) {
+				ws.send(JSON.stringify(results.multiFaceLandmarks));
+			}
+		}
+		canvasCtx.restore();
+	}
 </script>
 
 <div class="flex flex-col h-screen">
-  <div class="text-center bg-gray-800 text-white py-4 text-2xl font-bold">
-    VTuber Face Tracking
-  </div>
-  <button on:click={toggleCamera} class="p-4 text-lg font-bold">
-    {#if isCameraConnected} 
-      Disconnect Camera
-    {:else} 
-      Connect Camera 
-    {/if}
-  </button>  
-  <div class="flex flex-grow"> 
-    <div class="w-1/2 bg-gray-100 p-4 flex">
-      <video bind:this={videoElement} autoplay class="w-full h-full object-cover"></video>
-    </div>
-    <div class="w-1/2 bg-gray-200 p-4 flex items-center justify-center">
-      <iframe src="/WebGL/index.html" frameborder="0" class="w-full h-full object-cover"></iframe>
-    </div>
-  </div>
+	<div class="text-center bg-gray-800 text-white py-4 text-2xl font-bold">VTuber Face Tracking</div>
+	<button on:click={toggleCamera} class="p-4 text-lg font-bold">
+		{#if isCameraConnected}
+			Disconnect Camera
+		{:else}
+			Connect Camera
+		{/if}
+	</button>
+	<div class="flex flex-grow">
+		<div class="w-1/2 bg-gray-100 p-4 flex">
+			<video bind:this={videoElement} autoplay class="w-full h-full" />
+		</div>
+		<div class="w-1/2 bg-gray-200 p-4 flex items-center justify-center">
+			<iframe src="/WebGL/index.html" frameborder="0" class="w-full h-full object-cover" />
+		</div>
+	</div>
 </div>
-
-
-
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,32 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { Server } from 'socket.io';
+
+const webSocketServer = {
+  name: 'webSocketServer',
+  configureServer(server) {
+    if (!server.httpServer) return;
+
+    const io = new Server(server.httpServer);
+
+    console.log('WebSocket Server has been created'); 
+
+    io.on('connection', (socket) => {
+      console.log('A user connected'); 
+
+	  socket.emit('eventFromServer', 'Hello, World 👋');
+
+      socket.on('message', (message) => {
+        console.log('Received message:', message);
+      });      
+
+      socket.on('disconnect', () => {
+        console.log('A user disconnected'); // 사용자 연결 해제 시 로그 출력
+      });
+    });
+  }
+};
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit(), webSocketServer]
 });
